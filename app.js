3af7e1ab58950d92a9247f16d29dde0cfecc726c
Megalonnix <149280452+Megalonnix@users.noreply.github.com>
2025-02-07 17:34:55 -0300

    Funcionalidade de criação de lista HTML de amigos feita.

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. 
+// Aqui você deverá desenvolver a lógica para resolver o problema.
+
+const inputNmsDigit = document.getElementsByClassName('input-name')[0]
+const btnAdcAmigo = document.getElementsByClassName('button-add')[0]
+const btnSortAmigo = document.getElementsByClassName('button-draw')[0]
+const arrayAmigos = []
+
+const reloadListAmigosHtml = () => {
+    const listaAmigos = document.getElementById('listaAmigos')
+    listaAmigos.innerHTML = ''
+    
+    arrayAmigos.forEach(amigoRegistrado => {
+        const newIdxLista = document.createElement('li')
+        newIdxLista.innerText = amigoRegistrado
+        listaAmigos.appendChild(newIdxLista)
+    })
+}
+
+const adicionarAmigo = () => {
+    let nmDigit = inputNmsDigit.value
+    const nmNaoEAlgoVazio = nmDigit != null && nmDigit != undefined && nmDigit != ''
+    
+    nmNaoEAlgoVazio ?
+        arrayAmigos.push(nmDigit) :
+        window.alert('Digite algum nome por favor!')
+
+    inputNmsDigit.value = ''
+    reloadListAmigosHtml()
+}
+
+const sortearAmigo = () => {
+    return 0
+}
